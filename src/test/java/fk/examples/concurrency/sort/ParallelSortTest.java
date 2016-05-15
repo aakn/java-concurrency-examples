b73835b3acde65d71ffeec34dd2caf35cfcfadcf
Ali <ali.nalawala@flipkart.com>
2016-05-15 22:09:58 +0530

    Updated the tests

--- src/test/java/fk/examples/concurrency/sort/ParallelSortTest.java
+++ src/test/java/fk/examples/concurrency/sort/ParallelSortTest.java
@@ -21,23 +21,23 @@ public class ParallelSortTest {
   }
 
   @Test
+  public void shouldSortParallely() throws Exception {
+    log.info("Starting the parallel sort now");
+    int[] clone = numbers.clone();
+
+    Instant now = Instant.now();
+    Arrays.parallelSort(clone);
+    log.info("parallel sort done in {}", Duration.between(now, Instant.now()).toMillis());
+  }
+
+
+  @Test
   public void shouldSortNormally() throws Exception {
     log.info("Starting the sort now");
     int[] clone = numbers.clone();
 
     Instant now = Instant.now();
-    Arrays.sort(numbers);
-    log.info("normal sort done in {}", Duration.between(now, Instant.now()).toMillis());
-  }
-
-
-  @Test
-  public void shouldSortParallely() throws Exception {
-    log.info("Starting the parallel sort now {}");
-    int[] clone = numbers.clone();
-
-    Instant now = Instant.now();
-    Arrays.parallelSort(clone);
+    Arrays.sort(clone);
     log.info("normal sort done in {}", Duration.between(now, Instant.now()).toMillis());
   }
 }
